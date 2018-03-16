4443d70103af0c83420ed95222d580ace3a66281
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-17 03:32:54 +0530

    App controller architecture

---
+++ app.js
@@ -0,0 +1,16 @@
+// Storage Controller - LocalStorage
+
+// Item Controller - State of our Application
+const ItemCtrl = (function(){
+
+})();
+
+// UI Controller
+const UICtrl = (function(){
+
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl){
+
+})(ItemCtrl, UICtrl);
