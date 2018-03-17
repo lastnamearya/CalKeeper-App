562b923cffbaea5cd74dd9ab44ef5d9a26a9dad5
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-18 00:12:59 +0530

    Initalizing total Calories on load

--- app.js
+++ app.js
@@ -168,11 +168,6 @@ const App = (function(ItemCtrl, UICtrl){
       // Add Item to UI List
       UICtrl.addListItem(newItem);
 
-      // Get Total Calories
-      const totalCalories = ItemCtrl.getTotalCalories();
-      // Render total Calories Value in the UI
-      UICtrl.showTotalCalories(totalCalories);
-
       // Clear Inputs fields
       UICtrl.clearInput();
     }
@@ -194,6 +189,11 @@ const App = (function(ItemCtrl, UICtrl){
         // Send Data to UI for rendering ~ Populate List of Items
         UICtrl.populateItemList(items);
       }
+
+      // Get Total Calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Render total Calories Value in the UI
+      UICtrl.showTotalCalories(totalCalories);
 
       // Load event listeners
       loadEventListeners();
