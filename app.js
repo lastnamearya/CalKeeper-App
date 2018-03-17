b9cdef749a3912f4fa704ebcc635a8f4b99e2109
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-18 00:09:28 +0530

    Total Calories are rendering now

--- app.js
+++ app.js
@@ -47,6 +47,19 @@ const ItemCtrl = (function(){
 
       return newItem;
     },
+    getTotalCalories: function() {
+      let total = 0;
+
+      // Loop thorugh Data items and add calls
+      data.items.forEach(function(item){
+        total += item.calories;
+
+        // Set total cal in the data structure
+        data.totalCalories = total;
+      });
+      // Return total
+      return data.totalCalories;
+    },
     logData: function(){
       return data;
     }
@@ -62,7 +75,8 @@ const UICtrl = (function(){
     itemList: '#item-list',
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
-    itemCaloriesInput: '#item-calories'
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories'
   }
 
   // Public Methods
@@ -117,6 +131,9 @@ const UICtrl = (function(){
     hideList: function(){
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function(totalCalories){
+       document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
     getSelectors: function(){
       return UISelectors;
     }
@@ -148,9 +165,13 @@ const App = (function(ItemCtrl, UICtrl){
       // Add Item
       const newItem = ItemCtrl.addItem(input.name, input.calories);
 
-
       // Add Item to UI List
       UICtrl.addListItem(newItem);
+
+      // Get Total Calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Render total Calories Value in the UI
+      UICtrl.showTotalCalories(totalCalories);
 
       // Clear Inputs fields
       UICtrl.clearInput();
